08d6a446036961cdac0fcfa668bbc49ac7158759
HHHKKKHHH <1790733272@qq.com>
2025-08-04 23:20:48 +0800

    bugfix

--- main.py
+++ main.py
@@ -83,15 +83,8 @@ def load_from_mgf(filename: str):
     for pyteomics_spectrum in MGF(filename, convert_arrays=1):
         if pyteomics_spectrum == None:
             continue
-        # metadata = pyteomics_spectrum.get("params", None)
         mz = pyteomics_spectrum["m/z array"]
         intensities = pyteomics_spectrum["intensity array"]
-
-        # # Sort by mz (if not sorted already)
-        # if not np.all(mz[:-1] <= mz[1:]):
-        #     idx_sorted = np.argsort(mz)
-        #     mz = mz[idx_sorted]
-        #     intensities = intensities[idx_sorted]
 
         yield {"metadata": pyteomics_spectrum.get("params", None),
                "mz": mz,
